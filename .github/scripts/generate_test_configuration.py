65cc67ae8edaabdf949b243c3639325ef7c197cb
Bo Peng <peng.b.203@ms.saitama-u.ac.jp>
2023-08-30 12:03:26 +0900

    test ci

---
+++ .github/scripts/generate_test_configuration.py
@@ -0,0 +1,24 @@
+import os
+import yaml
+import subprocess
+
+# Get the list of modified or created files
+result = subprocess.run(["git", "diff", "--name-only", "HEAD^"], stdout=subprocess.PIPE)
+modified_files = result.stdout.decode().split("\n")
+
+# Create a new test configuration file
+new_workflow = {
+  "scenarios": []
+}
+
+# Add modified or created scenarios to the new test configuration file
+for file in modified_files:
+  if file.startswith("scenario_test_runner/scenario/") and file.endswith(".yaml"):
+    scenario_path = "$(find-pkg-share scenario_test_runner)/" + file
+    new_workflow["scenarios"].append({"path": scenario_path})
+
+# Write the new test configuration file
+new_workflow_file = os.path.join(os.path.dirname(__file__), '..', '..', 'scenario_test_runner', 'config', 'new_workflow.yaml')
+
+with open(new_workflow_file, 'w') as f:
+    yaml.dump(new_workflow, f)
